54d9ba63b07e517d4ee0b8f0fbfe88fd66c80cf8
Md Rashedul Alam Mazumder <yaan.uchiha29@gmail.com>
2019-06-16 01:58:42 +0600

    p o

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:porao_onboarding_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(OnBoardingApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
